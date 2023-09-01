a9c89bbdddb4f2cf261cc01bcf63c368bb0911f0
flinco <francodeciiudadela@gmail.com>
2023-09-01 14:03:46 -0300

    Added script

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(process.env.port, () =>
-  console.log(`Server Running on port ${process.env.port}`)
+app.listen(process.env.port || 5000, () =>
+  console.log(`Server Running on port ${process.env.port || 5000}`)
 );
 
 const contactEmail = nodemailer.createTransport({
