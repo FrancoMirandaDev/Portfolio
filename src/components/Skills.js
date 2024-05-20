c8b0b74e580a618bb72d3ac6c4d81ecf6e9e0459
flinco <francodeciiudadela@gmail.com>
2024-05-20 13:05:13 -0300

    Updated Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,21 +1,5 @@
 import { Row, Container, Col } from "react-bootstrap";
-
-import Img1 from "../assets/img/javascript.svg";
-import Img2 from "../assets/img/html5.svg";
-import Img3 from "../assets/img/css.svg";
-import Img4 from "../assets/img/react.svg";
-import Img5 from "../assets/img/nodejs.svg";
-import Img6 from "../assets/img/bootstrap.svg";
-import Img7 from "../assets/img/express.svg";
-import Img8 from "../assets/img/mongodb.svg";
-import Img9 from "../assets/img/nextjs-3.svg";
-import Img10 from "../assets/img/git.svg";
-import Img11 from "../assets/img/typescript.svg";
-import Img12 from "../assets/img/mysql.svg";
-import Img13 from "../assets/img/postgresql.svg";
-import Img14 from "../assets/img/redux.svg";
-import Img15 from "../assets/img/docker.svg";
-import Img16 from "../assets/img/aws.svg";
+import { skills } from "../helper/skills";
 
 import TrackVisibility from "react-on-screen";
 import "animate.css";
@@ -40,24 +24,7 @@ export const Skills = () => {
                       including...
                     </p>
                     <Row className="items">
-                      {[
-                        { img: Img1, title: "JavaScript" },
-                        { img: Img11, title: "TypeScript" },
-                        { img: Img2, title: "Html5" },
-                        { img: Img3, title: "Css3" },
-                        { img: Img5, title: "Node Js" },
-                        { img: Img4, title: "React Js" },
-                        { img: Img14, title: "Redux" },
-                        { img: Img6, title: "Bootstrap" },
-                        { img: Img7, title: "Express" },
-                        { img: Img9, title: "Next Js" },
-                        { img: Img12, title: "SQL" },
-                        { img: Img8, title: "MongoDB" },
-                        { img: Img13, title: "PostgreSQL" },
-                        { img: Img10, title: "Git" },
-                        { img: Img15, title: "Docker" },
-                        { img: Img16, title: "AWS" },
-                      ].map((item, index) => (
+                      {skills.map((item, index) => (
                         <Col
                           key={index}
                           className={
---
+++ src/helper/skills.js
@@ -0,0 +1,39 @@
+import Img1 from "../assets/img/javascript.svg";
+import Img2 from "../assets/img/html5.svg";
+import Img3 from "../assets/img/css.svg";
+import Img4 from "../assets/img/react.svg";
+import Img5 from "../assets/img/nodejs.svg";
+import Img7 from "../assets/img/express.svg";
+import Img8 from "../assets/img/mongodb.svg";
+import Img9 from "../assets/img/nextjs.svg";
+import Img10 from "../assets/img/git.svg";
+import Img11 from "../assets/img/typescript.svg";
+import Img12 from "../assets/img/mysql.svg";
+import Img13 from "../assets/img/postgresql.svg";
+import Img14 from "../assets/img/redux.svg";
+import Img15 from "../assets/img/docker.svg";
+import Img16 from "../assets/img/aws.svg";
+import Img17 from "../assets/img/python.svg";
+import Img18 from "../assets/img/django.svg";
+import Img19 from "../assets/img/kubernetes.svg";
+
+export const skills = [
+  { img: Img1, title: "JavaScript" },
+  { img: Img11, title: "TypeScript" },
+  { img: Img2, title: "Html5" },
+  { img: Img3, title: "Css3" },
+  { img: Img4, title: "React Js" },
+  { img: Img9, title: "Next Js" },
+  { img: Img14, title: "Redux" },
+  { img: Img5, title: "Node Js" },
+  { img: Img7, title: "Express" },
+  { img: Img17, title: "Python" },
+  { img: Img18, title: "Django" },
+  { img: Img12, title: "SQL" },
+  { img: Img8, title: "MongoDB" },
+  { img: Img13, title: "PostgreSQL" },
+  { img: Img19, title: "Kubernetes" },
+  { img: Img15, title: "Docker" },
+  { img: Img10, title: "Git" },
+  { img: Img16, title: "AWS" },
+];
